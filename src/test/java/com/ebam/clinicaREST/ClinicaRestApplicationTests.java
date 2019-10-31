ea77d1a93c52a693a087237f8b11f080651ff52a
eamadom2 <eamadom2@gmail.com>
2019-10-31 08:45:47 -0500

    Commit inicial

---
+++ src/test/java/com/ebam/clinicaREST/ClinicaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebam.clinicaREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
